06829bc6df2927952cdf9a875788ad856114e50a
neginasu <neginasu7@gmail.com>
2023-12-10 12:39:56 +0900

    feat: 全体のUIを作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <h1>Multiple Counter</h1>
+  <div>
+      <div class="counter">
+        <input
+          type="text"
+         />
+        <button class="increment">+</button>
+        <button class="decrement">-</button>
+        <button class="reset">0</button>
+        <button class="remove">x</button>
+      </div>
+        <button class="newCounter">new counter</button>
+    <div>
+      <strong>title list:</strong>
+    </div>
+    <div>
+      <strong>sum of count:</strong>
+    </div>
+  </div>
+</div>
